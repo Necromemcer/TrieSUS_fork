eb8fceb13ff5404216eeb12ae8ae3e83219faf22
Platon Baturin <platon@hl-codon-19-04.ebi.ac.uk>
2024-08-02 14:06:14 +0100

    Corrected and finalized cover_set_extended, removed files with prior experiments

--- triesus/cover_set_extended.py
+++ triesus/cover_set_extended.py
@@ -8,55 +8,27 @@ def solve_cover_set(input_sets):
     model = cp_model.CpModel()
     solver = cp_model.CpSolver()
     # Create a binary variable for each set in the input
-    set_vars = {}
-    for set_name in input_sets:
-        set_vars[set_name] = model.NewBoolVar(set_name)
+    set_vars = {set_name: model.NewBoolVar(set_name) for set_name in input_sets}
     # Create constraints to cover all elements with the selected sets
-    for element in set.union(*input_sets.values()):
-        model.AddBoolOr(
-            [
-                set_vars[set_name]
-                for set_name, element_set in input_sets.items()
-                if element in element_set
-            ]
-        )
+    all_elements = set.union(*input_sets.values())
+    for element in all_elements:
+        model.AddBoolOr([set_vars[set_name] for set_name, element_set in input_sets.items() if element in element_set])
     # Create the objective to minimize the number of selected sets
     objective = sum(set_vars.values())
     model.Minimize(objective)
-    # Callback to store all solutions
-    class SolutionCollector(cp_model.CpSolverSolutionCallback):
-        def __init__(self, set_vars):
-            cp_model.CpSolverSolutionCallback.__init__(self)
-            self.set_vars = set_vars
-            self.solutions = []
-        
-        def on_solution_callback(self):
-            selected_sets = [
-                set_name for set_name, var in self.set_vars.items() if self.Value(var) == 1
-            ]
-            self.solutions.append(selected_sets)
-            #print(selected_sets)
-    all_solutions = []
-    min_length = float('inf')
-    found_all_solutions = True
-    while found_all_solutions:
-        # Create the solution collector
-        solution_collector = SolutionCollector(set_vars)
-        # Solve the model with the solution callback
-        status = solver.SolveWithSolutionCallback(model, solution_collector)
-        #print(status)
-        if status == cp_model.OPTIMAL:
-            ### Keep only the shortest solutions
-            for solution in solution_collector.solutions:
-                solution_length = len(solution)
-                if solution_length < min_length:
-                    all_solutions = [solution]
-                    min_length = solution_length
-                elif solution_length == min_length:
-                    all_solutions.append(solution)
-            # Exclude the current solution to find a  new one
-            model.Add(sum(set_vars[set_name] for set_name in solution_collector.solutions[-1]) <= len(solution_collector.solutions[-1]) - 1)
-        else:
-            found_all_solutions = False
-    # Return all collected solutions and the status
-    return all_solutions, status
+    # Collect all optimal solutions
+    optimal_solutions = []
+    early_stop = 0
+    status = solver.Solve(model)
+    while early_stop == 0 and status == cp_model.OPTIMAL:
+        current_solution = [set_name for set_name, var in set_vars.items() if solver.Value(var) == 1]
+        optimal_solutions.append(current_solution)
+        # Add a constraint to exclude the current solution to find new ones
+        model.Add(sum(set_vars[set_name] for set_name in current_solution) <= len(current_solution) - 1)
+        status = solver.Solve(model)
+        if status != cp_model.OPTIMAL:
+            break
+        next_solution = [set_name for set_name, var in set_vars.items() if solver.Value(var) == 1]
+        # Check for early stop
+        early_stop += len(current_solution) - len(next_solution)
+    return optimal_solutions, status
